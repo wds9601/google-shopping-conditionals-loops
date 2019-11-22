19dfe15a44284f9bb42d61baf75bf4576c52d730
William Downey-Schell <Will@Williams-MacBook-Air.local>
2019-11-22 09:50:03 -0800

    attmepted

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -4,22 +4,35 @@
 let data = require('../products.json')
 
 // This is a print out of all the items in the data
-console.log(data.items)
+// console.log(data.items)
 
 // Next, it's recommended to just look at the first item
-console.log(data.items[0])
+// console.log(data.items[0])
 
 // YOUR CODE BELOW
 // 1.) Go through the `items` and find all results that have `kind` of
 // `shopping#product`. Print the count of these results. Where else is
 // this count information stored in the search results?
 
-console.log('\nProblem 1:\n')
+// console.log('\nProblem 1:\n')
 
-// 2.) Print the `title` all items with a `backorder` availability
+// let productArray = data.items.filter((item) => {
+//     return item.kind === 'shopping#product'
+// })
+// console.log(productArray.length)
+
+
+// 2.) Print the `title` of all items with a `backorder` availability
 // in `inventories`.
 
 console.log('\nProblem 2:\n')
+
+let backorderArray = data.items.inventories.filter((item) => {
+    return item.availability === 'backorder'
+    // let backorderArray = inventoryArray.filter
+})
+
+console.log(backorderArray)
 
 // 3.) Print the `title` all items with more than one image link.
 
